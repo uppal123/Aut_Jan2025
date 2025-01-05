cf7ed9215c3d8da271fba62666a26870d2be2195
poojauppal <pujapuja35@gmail.com>
2025-01-05 14:07:44 +0530

    jan 2025

---
+++ src/Duplicate_char.java
@@ -0,0 +1,19 @@
+public class Duplicate_char {
+    String str = "pooja";
+    char[] ch = str.toCharArray();
+
+    public void search(){
+        for(int i= 0; i< str.length(); i++){
+            for(int j= i+1; j<str.length(); j++){
+                if(ch[i]== ch[j]){
+                    System.out.println("Duplicate char is " + ch[j]);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Duplicate_char dc = new Duplicate_char();
+        dc.search();
+    }
+}
