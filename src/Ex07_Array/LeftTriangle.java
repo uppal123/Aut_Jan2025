d4e8f2cacff0e2f22aa8af09c8017606ca3e80d6
poojauppal <pujapuja35@gmail.com>
2025-02-10 18:56:50 +0530

    Task 9th feb

---
+++ src/Ex07_Array/LeftTriangle.java
@@ -0,0 +1,12 @@
+package Ex07_Array;
+
+public class LeftTriangle {
+    public static void main(String[] args) {
+        for(int i=0; i<=6; i++){
+            for(int j=6; j>=i; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
