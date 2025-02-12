ac4285794e3fcd57dbd5bf290aed223100c12474
poojauppal <pujapuja35@gmail.com>
2025-02-13 01:34:55 +0530

    Task 9th feb

---
+++ src/Ex07_Array/SortArray2.java
@@ -0,0 +1,21 @@
+package Ex07_Array;
+
+public class SortArray2 {
+    public static void main(String[] args) {
+        int[] n = {12, 34, 10, 1, 100, 3, 4, 32};
+        int i;
+        for (i = 0; i <= n.length - 1; i++) {
+            for (int j = 0; j <= n.length - 1; j++) {
+                if (n[j] > n[i]) {
+                    int temp = n[i];
+                    n[i] = n[j];
+                    n[j] = temp;
+                }
+            }
+        }
+        for (int num : n) {
+            System.out.print(num + " ");
+
+        }
+    }
+}
