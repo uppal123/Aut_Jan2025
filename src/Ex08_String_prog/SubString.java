808b5f865553af871955d77e5691dd5a30e71436
poojauppal <pujapuja35@gmail.com>
2025-02-22 21:55:48 +0530

    22nd feb

---
+++ src/Ex08_String_prog/SubString.java
@@ -0,0 +1,10 @@
+package Ex08_String_prog;
+
+public class SubString {
+    public static void main(String[] args) {
+        String str = "Aeroplane";
+        String sub = str.substring(6, 9);
+        System.out.println(sub);
+
+    }
+}
