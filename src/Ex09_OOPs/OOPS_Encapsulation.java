50a3b193f7fd2b39b1bf9a94ffe682e0a330e6d7
uppal123 <127531731+uppal123@users.noreply.github.com>
2025-03-06 01:21:06 +0530

    Adding file for ecapsulation test (#1)
    * Adding file for ecapsulation test
    
    * update
    
    * new update

---
+++ src/Ex09_OOPs/OOPS_Encapsulation.java
@@ -0,0 +1,11 @@
+package Ex09_OOPs;
+
+public class OOPS_Encapsulation {
+    public static void main(String[] args) {
+        System.out.println("Pooja");
+
+        System.out.println("sourabh");
+    }
+}
+
+//git push --set-upstream origin new-branch
