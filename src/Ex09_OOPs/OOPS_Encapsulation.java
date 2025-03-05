4915e28b3029376925475c86ec52027a03ae2cf6
poojauppal <pujapuja35@gmail.com>
2025-03-06 02:00:45 +0530

    minor change

--- src/Ex09_OOPs/OOPS_Encapsulation.java
+++ src/Ex09_OOPs/OOPS_Encapsulation.java
@@ -11,9 +11,10 @@ public class OOPS_Encapsulation {
 //git push --set-upstream origin new-branch - kahaan bhejna hai
 //git checkout -b oops_programs origin/master
 
-// git branch branch_name
-//git push --set-upstream origin branch_name - kahaan bhejna hai
-// git checkout branch_name
+// git branch branch_name //to create a branch
+//git push --set-upstream origin branch_name - to track a branch
+// git checkout branch_name // to change a branch
+
 
 //git status
 //git add .
