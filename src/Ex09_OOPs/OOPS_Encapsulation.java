9a7325374191f5b17d8bb5183c9b43a52a229f06
poojauppal <pujapuja35@gmail.com>
2025-03-06 01:19:56 +0530

    new update

--- src/Ex09_OOPs/OOPS_Encapsulation.java
+++ src/Ex09_OOPs/OOPS_Encapsulation.java
@@ -3,5 +3,9 @@ package Ex09_OOPs;
 public class OOPS_Encapsulation {
     public static void main(String[] args) {
         System.out.println("Pooja");
+
+        System.out.println("sourabh");
     }
 }
+
+//git push --set-upstream origin new-branch
